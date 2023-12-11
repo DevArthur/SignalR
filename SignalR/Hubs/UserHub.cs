67343b9305e58444ffe8aae22cb335027372c17c
DevArthur <dev.lopez.alcaraz@gmail.com>
2023-12-11 14:52:39 -0600

    added  NewWindowLoaded() method to UserHub

--- SignalR/Hubs/UserHub.cs
+++ SignalR/Hubs/UserHub.cs
@@ -4,4 +4,20 @@ namespace SignalR.Hubs;
 
 public class UserHub : Hub
 {
+    // Counts the total views that a Web page has
+    public int TotalViews { get; set; } = 0;
+
+    /// <summary>
+    /// Increments the TotalViews variable.
+    /// </summary>
+    /// <returns>Task.</returns>
+    public async Task NewWindowLoaded()
+    {
+        TotalViews++;
+
+        // sends update to all clients that total views have been updated
+        await Clients.All.SendAsync("updateTotalViews", TotalViews);
+    }
+
+
 }
